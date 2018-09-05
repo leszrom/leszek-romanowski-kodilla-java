19979b6c8f93511b4a7b6cfd8af0423c2b695155
leszrom <romanowski.leszek@gmail.com>
2018-09-05 13:36:35 +0200

    first testing

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+}
