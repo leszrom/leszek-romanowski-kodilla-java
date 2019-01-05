8c9679c93ac62c55c260facd5baf27d03b2b78fd
leszrom <romanowski.leszek@gmail.com>
2019-01-05 16:13:30 +0100

    Write a test for pizza decorators call in cascade

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/decorator/pizza/PizzaOrderTestSuite.java
@@ -24,4 +24,23 @@ public class PizzaOrderTestSuite {
         Assert.assertEquals(BASIC_DESCRIPTION.concat("\n+ cheese"), description);
     }
 
+    @Test
+    public void should_return_cost_and_description_of_pizza_with_cheese_cactus_and_chocolate() {
+        //Given
+        PizzaOrder theOrder = new BasicPizzaOrder();
+        theOrder = new CheeseDecorator(theOrder);
+        theOrder = new CactusDecorator(theOrder);
+        theOrder = new ChocolateDecorator(theOrder);
+
+        //When
+        BigDecimal cost = theOrder.getCost();
+        String description = theOrder.getDescription();
+
+        //Then
+        Assert.assertEquals(new BigDecimal(36), cost);
+        Assert.assertEquals(BASIC_DESCRIPTION
+                .concat("\n+ cheese")
+                .concat("\n+ cactus in the middle")
+                .concat("\n+ chocolate covered"), description);
+    }
 }
