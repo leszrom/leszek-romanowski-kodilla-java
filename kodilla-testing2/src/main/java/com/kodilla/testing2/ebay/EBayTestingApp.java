a706912feb9eef612b20ff1a8710cfed2ce397b9
leszrom <romanowski.leszek@gmail.com>
2019-02-07 07:24:42 +0100

    Searching 'Laptop' on eBay

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,5 +1,16 @@
 package com.kodilla.testing2.ebay;
 
-public class EBayTestingApp {
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+public class EBayTestingApp {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com/");
+        WebElement searchField = driver.findElement(By.id("gh-ac"));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
 }
